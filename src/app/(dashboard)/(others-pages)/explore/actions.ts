2718894b686fbb6cc3fdc385d2fe96655fda2ab5
hans schilling <hasp1988@gmail.com>
2025-07-09 19:25:15 +0100

    changed getExploreVacancies action

--- src/app/(dashboard)/(others-pages)/explore/actions.ts
+++ src/app/(dashboard)/(others-pages)/explore/actions.ts
@@ -1,11 +1,37 @@
 'use server'
+import { cookies } from 'next/headers'
 
 import { JOBS_API_URL } from '@/common/constants'
-import { get, post } from '@/common/util/fetch'
+import { get, getHeaders, post } from '@/common/util/fetch'
+
+// export const getExploreVacancies = async () => {
+//   console.log('FETCH REQUEST: ', JOBS_API_URL, '/jobs/explore')
+//   const data = await get(JOBS_API_URL, 'jobs/explore')
+//   if (data.error) {
+//     throw new Error(data.error.message)
+//   }
+
+//   return data
+// }
 
 export const getExploreVacancies = async () => {
-  console.log('FETCH REQUEST: ', JOBS_API_URL, '/jobs/explore')
-  const data = await get(JOBS_API_URL, 'jobs/explore')
+  const headers = await getHeaders()
+
+  console.log('FETCH REQUEST:', JOBS_API_URL, '/jobs/explore')
+
+  const res = await fetch(`${JOBS_API_URL}/jobs/explore`, {
+    headers,
+    credentials: 'include',
+  })
+
+  if (!res.ok) {
+    const errorText = await res.text()
+    console.error('Fetch failed:', res.status, errorText)
+    throw new Error(`HTTP error! ${res.status} ${res.statusText}`)
+  }
+
+  const data = await res.json()
+
   if (data.error) {
     throw new Error(data.error.message)
   }
